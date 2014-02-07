daca58f35ca304231884e78f6ff343ad1b1c6ce1
Alex Savchenko <a.savchenko@codetiburon.com>
2014-02-07 13:18:48 +0200

    set up test env

---
+++ tests/QtzCommonTest/Validator/RemoteContentExistsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace QtzCommonTest;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Description of RemoteContentExistsTest
+ *
+ * @author alex
+ */
+class RemoteContentExistsTest extends PHPUnit_Framework_TestCase
+{
+    public function testCanRun()
+    {
+        $this->assertTrue(true);
+    }
+}
